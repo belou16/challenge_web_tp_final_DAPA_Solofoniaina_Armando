10289e4656e8d21a927c3cd9f3fbc9ed16d18844
Solofoniaina Armando <armandodapa10@gmail.com>
2026-01-16 11:49:34 +0100

    Gestion du catalogue

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/marketplace_db')
     .catch(err => console.error(err));
 
 
-// 1 . Modèle pour Category
+// création des modèles
+
+    // 1 . Modèle pour Category
 const categorySchema = new mongoose.Schema({
     nom: { type: String, required: true }
 });
@@ -22,7 +24,7 @@ const productSchema = new mongoose.Schema({
     nom: { type: String, required: true },
     prix: { type: Number, required: true },
     stock: { type: Number, required: true},
-    categorie: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
 });
 const Product = mongoose.model('Product', productSchema);
 
@@ -42,4 +44,40 @@ const reviewSchema = new mongoose.Schema({
     auteur: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 const Review = mongoose.model('Review', reviewSchema);
+
+
+
+// création des méthodes
+
+// 1 . Gestion du catalogue
+
+// GET product -> Retourne tous les produits avec le détail de leur catégorie.
+app.get('/api/product', async (req, res) => {
+    const product = await Product.find();
+    res.json(product);
+});
+
+// POST product -> Ajoute un produit (Vérifier que le prix est positif).
+app.post('/api/product', async (req, res) => {
+    if (req.body.prix < 0) {
+    return res.status(400).json({ message: "prix inférieur à 1" });
+}
+const product = new Product(req.body);
+await product.save();
+res.status(201).json(product);
+});
+
+// --- DÉMARRAGE DU SERVEUR ---
+app.listen(3000, () => {
+    console.log("Serveur démarré sur http://localhost:3000");
+});
+
+
+
+
+
+
+
+
+
 
